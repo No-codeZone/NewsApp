a9c8e1a96f4672e49d3f720a1d5833b72805338c
No-codeZone <134377676+No-codeZone@users.noreply.github.com>
2024-11-29 13:18:10 +0530

    var changes

--- app/src/main/java/com/example/newsapp/utils/constants.java
+++ app/src/main/java/com/example/newsapp/utils/constants.java
@@ -7,8 +7,7 @@ import java.util.List;
 
 public class constants {
     public static String BASE_URL = "https://newsapi.org/v2/";
-    public static String API_KEY = "066af46afd694425837ed014234fb786";
+    public static String API_KEY = "YOUR_API_KEY";
     public static final String STATIC_USERNAME = "User@Android";
     public static final String STATIC_PASSWORD = "12345";
-    //    public  static  String API_KEY = "44d79a75448e426b8d98f135f0b3c536";
 }
